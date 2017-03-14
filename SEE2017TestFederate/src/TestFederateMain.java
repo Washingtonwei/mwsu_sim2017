af19655acf5162cef24e26068de82b98de0902bb
csos95 <csoscss@gmail.com>
2017-03-14 16:03:13 -0500

    Add gui to test federate.
    The gui has join, resign, and reset buttons.

--- SEE2017TestFederate/src/TestFederateMain.java
+++ SEE2017TestFederate/src/TestFederateMain.java
@@ -1,47 +1,100 @@
 import java.io.File;
-import java.util.Scanner;
+
+import java.awt.*; // Using AWT container and component classes
+import java.awt.event.*; // Using AWT event classes and listener interfaces
 
 import skf.config.Configuration;
 import skf.config.ConfigurationFactory;
 import federate.TestFederate;
 import federate.TestFederateAmbassador;
 
-public class TestFederateMain {
-
-
+// An AWT program inherits from the top-level container java.awt.Frame
+public class TestFederateMain extends Frame implements ActionListener {
 	private static final File conf = new File("conf/conf.json");
+	private static Configuration configuration;
+	private static TestFederate federate;
 
-	private static Scanner sc = null;
+	private Label name;
+	private Button reset;
+	private Button join;
+	private Button resign;
+	private TextField status;
+
+	// Constructor to setup GUI components and event handlers
+	public TestFederateMain() {
+		setLayout(new GridLayout(3, 2));
+
+		name = new Label("MWSU Satellite Federate");
+		add(name);
+
+		reset = new Button("Reset");
+		add(reset);
+
+		join = new Button("Join");
+		add(join);
+
+		resign = new Button("Resign");
+		add(resign);
+
+		reset.addActionListener(this);
+		join.addActionListener(this);
+		resign.addActionListener(this);
+
+		status = new TextField("Welcome to the MWSU Satellite Federate", 10);
+		status.setEditable(false);
+		add(status);
+
+		setTitle("MWSU Satellite Federate");
+		setSize(600, 300);
+
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we) {
+				try {
+					federate.diconnectFromRTI();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				dispose();
+			}
+		});
+
+		setVisible(true);
+	}
 
 	public static void main(String[] args) throws Exception {
-
 		ConfigurationFactory factory = new ConfigurationFactory();
-		Configuration configuration = factory.importConfiguration(conf);
+		configuration = factory.importConfiguration(conf);
 
 		TestFederateAmbassador amb = new TestFederateAmbassador();
-		TestFederate federate = new TestFederate(amb);
+		federate = new TestFederate(amb);
 
-		federate.configureAndStart(configuration);
+		new TestFederateMain();
+	}
 
-		sc = new Scanner(System.in);
-		String currValue = null;
-		while(true){
-			System.out.println("enter command: ");
-			currValue = sc.next();
-			System.out.println("command: " + currValue);
-			
-			if(currValue.equals("a")){
-				federate.sendGoToShutdown();
-				break;
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		switch (event.getActionCommand()) {
+		case "Join":
+			try {
+				federate.configureAndStart(configuration);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-
-			if(currValue.equals("q")){
-				System.out.println("shutting down");
+			break;
+		case "Resign":
+			try {
 				federate.diconnectFromRTI();
-				break;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			break;
+		case "Reset":
+			TestFederateAmbassador amb = new TestFederateAmbassador();
+			try {
+				federate = new TestFederate(amb);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
-
-	}//main
-
+	}
 }
--- SEE2017TestFederate/src/federate/TestFederate.java
+++ SEE2017TestFederate/src/federate/TestFederate.java
@@ -27,19 +27,19 @@ import skf.exception.UnsubscribeException;
 import skf.exception.UpdateException;
 
 public class TestFederate extends SEEAbstractFederate implements Observer {
-	
+
 	private final static Logger logger = LogManager.getLogger(TestFederate.class);
-	
+
 	private String LOCAL_SETTINGS_DESIGNATOR = null;
-	
+
 	private ModeTransitionRequest modeTransition = null;
-	
+
 	private ExecutionConfiguration exco = null;
-	
+
 	private HLAModule hla_module = null;
-	
+
 	private ShutdownTask shutdownTask = null;
-	
+
 	private long timeCycle;
 	private Orbit orbit;
 
@@ -50,37 +50,36 @@ public class TestFederate extends SEEAbstractFederate implements Observer {
 		super(seefedamb);
 		this.modeTransition = new ModeTransitionRequest();
 		this.exco = new ExecutionConfiguration();
-		this.hla_module  = new HLAModule(seefedamb);
+		this.hla_module = new HLAModule(seefedamb);
 		this.shutdownTask = new ShutdownTask(this);
 	}
-	
-	public void configureAndStart(Configuration config) throws ConnectionFailed, InvalidLocalSettingsDesignator, 
-														UnsupportedCallbackModel, CallNotAllowedFromWithinCallback, RTIinternalError, 
-														CouldNotCreateLogicalTimeFactory, FederationExecutionDoesNotExist, InconsistentFDD, 
-														ErrorReadingFDD, CouldNotOpenFDD, SaveInProgress, RestoreInProgress, NotConnected, 
-														MalformedURLException, FederateNotExecutionMember, InstantiationException, 
-														IllegalAccessException, NameNotFound, InvalidObjectClassHandle, AttributeNotDefined, 
-														ObjectClassNotDefined, SubscribeException, UnsubscribeException, 
-														InvalidInteractionClassHandle, InteractionClassNotDefined, InteractionClassNotPublished, 
-														InteractionParameterNotDefined, PublishException {
-		
+
+	public void configureAndStart(Configuration config)
+			throws ConnectionFailed, InvalidLocalSettingsDesignator, UnsupportedCallbackModel,
+			CallNotAllowedFromWithinCallback, RTIinternalError, CouldNotCreateLogicalTimeFactory,
+			FederationExecutionDoesNotExist, InconsistentFDD, ErrorReadingFDD, CouldNotOpenFDD, SaveInProgress,
+			RestoreInProgress, NotConnected, MalformedURLException, FederateNotExecutionMember, InstantiationException,
+			IllegalAccessException, NameNotFound, InvalidObjectClassHandle, AttributeNotDefined, ObjectClassNotDefined,
+			SubscribeException, UnsubscribeException, InvalidInteractionClassHandle, InteractionClassNotDefined,
+			InteractionClassNotPublished, InteractionParameterNotDefined, PublishException {
+
 		// -------------------- Federation Join Process -------------------- //
 		System.out.println("1");
 		/*
-		 *  1. Start
-		 *  Configure the SKF core components
+		 * 1. Start Configure the SKF core components
 		 */
 		super.configure(config);
 		super.subscribeSubject(this);
 
 		System.out.println("2");
 		/*
-		 * 2. Connect to RTI 
+		 * 2. Connect to RTI
 		 * 
-		 * For VT MAK LOCAL_SETTINGS_DESIGNATOR = "";
-		 * For Pitch RTI LOCAL_SETTINGS_DESIGNATOR = "crcHost=" + <crcHost> + "\ncrcPort=" + <crcPort>;
+		 * For VT MAK LOCAL_SETTINGS_DESIGNATOR = ""; For Pitch RTI
+		 * LOCAL_SETTINGS_DESIGNATOR = "crcHost=" + <crcHost> + "\ncrcPort=" +
+		 * <crcPort>;
 		 */
-		LOCAL_SETTINGS_DESIGNATOR = "crcHost="+config.getCrcHost()+"\ncrcPort="+config.getCrcPort();
+		LOCAL_SETTINGS_DESIGNATOR = "crcHost=" + config.getCrcHost() + "\ncrcPort=" + config.getCrcPort();
 		super.connectOnRTI(LOCAL_SETTINGS_DESIGNATOR);
 
 		System.out.println("3");
@@ -88,19 +87,20 @@ public class TestFederate extends SEEAbstractFederate implements Observer {
 		 * 3. Join the Federation Execution
 		 */
 		super.joinIntoFederationExecution();
-		
+
 		/**
-		 * 4. Enable Asynchronous Delivery 
-		 * Already done through the use of the 'configuration.json' file.
-		 * Please make sure that in the 'configuration.json' file the 'asynchronousDelivery' attribute is set to 'true'
+		 * 4. Enable Asynchronous Delivery Already done through the use of the
+		 * 'configuration.json' file. Please make sure that in the
+		 * 'configuration.json' file the 'asynchronousDelivery' attribute is set
+		 * to 'true'
 		 */
-		
-		
-		// -------------------- Federate Initialization Process -------------------- //
-		
+
+		// -------------------- Federate Initialization Process
+		// -------------------- //
+
 		/**
-		 * 1. Setup RTI Handles
-		 * Not needed because it is handled by the skf core components
+		 * 1. Setup RTI Handles Not needed because it is handled by the skf core
+		 * components
 		 */
 
 		System.out.println("4");
@@ -113,7 +113,7 @@ public class TestFederate extends SEEAbstractFederate implements Observer {
 		/**
 		 * 3. Wait for ExCO Discovery
 		 */
-		//super.waitForElementDiscovery(ExecutionConfiguration.class);
+		// super.waitForElementDiscovery(ExecutionConfiguration.class);
 
 		System.out.println("6");
 		/*
@@ -126,7 +126,7 @@ public class TestFederate extends SEEAbstractFederate implements Observer {
 		 * 5. Wait for ExCO Update and Check for Mode Transition to Shutdown.
 		 * The callback is managed by the 'update' method
 		 */
-		//super.waitForAttributeValueUpdate(ExecutionConfiguration.class);
+		// super.waitForAttributeValueUpdate(ExecutionConfiguration.class);
 
 		System.out.println("8");
 		/**
@@ -136,29 +136,28 @@ public class TestFederate extends SEEAbstractFederate implements Observer {
 
 		System.out.println("9");
 		/**
-		 * 7. Publish and Subscribe Federate Object Class Attributes and Interaction Classes
-		 * 8. Reserve All Federate Object Instance Names
-		 * 9. Wait for All Federate Object Instance Name Reservation Success/Failure Callbacks
-		 * 10. Register Federate Object Instances
-		 * 11. Wait for All Required Objects to be Discovered
+		 * 7. Publish and Subscribe Federate Object Class Attributes and
+		 * Interaction Classes 8. Reserve All Federate Object Instance Names 9.
+		 * Wait for All Federate Object Instance Name Reservation
+		 * Success/Failure Callbacks 10. Register Federate Object Instances 11.
+		 * Wait for All Required Objects to be Discovered
 		 *
-		 * Use here: 
-		 * -  super.subscribeElement(objectClass);
-		 * -  super.subscribeInteraction(interactionClass);
+		 * Use here: - super.subscribeElement(objectClass); -
+		 * super.subscribeInteraction(interactionClass);
 		 */
 		super.subscribeReferenceFrame(FrameType.MoonCentricFixed);
-		
+
 		/**
-		 * 12. Setup HLA Time Management: Enable Time Constrained Disable Time Regulating
-		 * Already done by using the 'configuration.json' file.
-		 * Please make sure that in the 'configuration.json' file the:
-		 * - 'timeConstrained' attribute is set to 'true'
-		 * - 'timeRegulating' attribute is set to 'false'
+		 * 12. Setup HLA Time Management: Enable Time Constrained Disable Time
+		 * Regulating Already done by using the 'configuration.json' file.
+		 * Please make sure that in the 'configuration.json' file the: -
+		 * 'timeConstrained' attribute is set to 'true' - 'timeRegulating'
+		 * attribute is set to 'false'
 		 */
-		
+
 		/**
-		 * 13. Query GALT and Time Advance to GALT
-		 * Already done by the skf core components
+		 * 13. Query GALT and Time Advance to GALT Already done by the skf core
+		 * components
 		 */
 
 		System.out.println("10");
@@ -166,58 +165,61 @@ public class TestFederate extends SEEAbstractFederate implements Observer {
 		 * 14. Goto Execution
 		 */
 		super.startExecution();
-		
+
 		timeCycle = getTime().getFederationExecutionTimeCycle();
 		orbit = new Orbit();
 
 		last_theta = orbit.getTrueAnomaly();
 		last_radius = orbit.getRadius();
 	}
-	
-	//this is where we will propagate satellites
+
+	// this is where we will propagate satellites
 	int i = 0;
+
 	@Override
 	protected void doAction() {
-		//get the current time cycle, calculate the difference (should always be 1000000) and propagate by that amount of time
+		// get the current time cycle, calculate the difference (should always
+		// be 1000000) and propagate by that amount of time
 		long temp = getTime().getFederationExecutionTimeCycle();
 		long timeDifference = temp - timeCycle;
 		timeCycle = temp;
-		//need to change orbit class so it fits our satellite/moon scenario
+		// need to change orbit class so it fits our satellite/moon scenario
 		if (orbit != null) {
-			//orbits per day / seconds in a day / microseconds in a second * the time difference
+			// orbits per day / seconds in a day / microseconds in a second *
+			// the time difference
 			orbit.Propogate(12.62256095 / 86400 / 1000000 * timeDifference);
-			System.out.println(String.format("Time: %1$d rDiff: %2$fm tDiff: %3$f°", i, 
+			System.out.println(String.format("Time: %1$d rDiff: %2$fm tDiff: %3$f°", i,
 					Math.abs(last_radius - orbit.getRadius()), Math.abs(last_theta - orbit.getTrueAnomaly())));
 			last_theta = orbit.getTrueAnomaly();
 			last_radius = orbit.getRadius();
 		}
 		/*
-		 *  proactive behavior
+		 * proactive behavior
 		 */
 		logger.log(Level.INFO, "step: " + (i++));
-		
+
 	}
 
-	//this is where we will respond to messages (i think)
+	// this is where we will respond to messages (i think)
 	@Override
 	public void update(Observable o, Object arg) {
-		
+
 		/*
-		 *  reactive behavior
+		 * reactive behavior
 		 */
-		if(arg instanceof ExecutionConfiguration){
+		if (arg instanceof ExecutionConfiguration) {
 			logger.log(Level.INFO, "**** Update ExecutionConfiguration ****");
-			exco = ((ExecutionConfiguration)arg);
-			if(exco.getNext_execution_mode() == ExecutionMode.EXEC_MODE_SHUTDOWN)
+			exco = ((ExecutionConfiguration) arg);
+			if (exco.getNext_execution_mode() == ExecutionMode.EXEC_MODE_SHUTDOWN)
 				new Thread(shutdownTask).start();
-			
+
 		}
-		
-		if(arg instanceof ReferenceFrame){
+
+		if (arg instanceof ReferenceFrame) {
 			logger.log(Level.INFO, "**** Update ReferenceFrame ****");
-			logger.log(Level.INFO, (ReferenceFrame)arg);
+			logger.log(Level.INFO, (ReferenceFrame) arg);
 		}
-		
+
 	}
 
 	public HLAModule getHLAModule() {
@@ -229,7 +231,9 @@ public class TestFederate extends SEEAbstractFederate implements Observer {
 		modeTransition.setExecution_mode(MTRMode.MTR_GOTO_SHUTDOWN);
 		try {
 			super.updateInteraction(modeTransition);
-		} catch (UpdateException | InteractionClassNotPublished | InteractionParameterNotDefined | InteractionClassNotDefined | SaveInProgress | RestoreInProgress | FederateNotExecutionMember | NotConnected | RTIinternalError e) {
+		} catch (UpdateException | InteractionClassNotPublished | InteractionParameterNotDefined
+				| InteractionClassNotDefined | SaveInProgress | RestoreInProgress | FederateNotExecutionMember
+				| NotConnected | RTIinternalError e) {
 			e.printStackTrace();
 		}
 
